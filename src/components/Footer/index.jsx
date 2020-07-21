43599f26c603391e808f8b7e61351fc13b58a6f2
Rendy <rendy.de.p@gmail.com>
2020-07-21 21:21:02 +0700

    Revamp Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,12 +5,8 @@ const Footer = (props) => {
     return (
         <footer className='footer'>
             <div className='frow-container'>
-                <div className='inner'>
-                    <div className='frow'>
-                        <div className='col-sm-6-7 col-md-4-5 col-lg-3-4'>
-                            {props.children}
-                        </div>
-                    </div>
+                <div className='inner text-center'>
+                    {props.children}
                 </div>
             </div>
         </footer>
